df77e78049cddbf534c83d642bd4d4189be44de5
Pedro Máximo Campos do Carmo <1273582@sga.pucminas.br>
2023-10-04 22:25:11 -0300

    estacionamento parte 1

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -3,24 +3,36 @@
 public class Estacionamento {
 
 	private String nome;
-	private Cliente[] id;
-	private Vaga[] vagas;
+	private List<Cliente> clientes = new ArrayList<>();
+	private List<Vaga> vagas = new ArrayList<>();
 	private int quantFileiras;
 	private int vagasPorFileira;
 
 	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-		
+		this.nome=nome;
+		this.quantfileiras=fileiras;
+		this.vagasPorFileira=vagasPorFila;
 	}
 
 	public void addVeiculo(Veiculo veiculo, String idCli) {
+		Cliente buscando = new Cliente(idCli);
+		for(Cliente x:clientes){
+			if(x.equals(buscando))
+				x.addVeiculo(veiculo);
+			}
+		}
 		
 	}
 
 	public void addCliente(Cliente cliente) {
-		
+		clientes.add(cliente);
+
+
 	}
 
 	private void gerarVagas() {
+
+		
 		
 	}
 
@@ -41,10 +53,19 @@ public class Estacionamento {
 	}
 
 	public double valorMedioPorUso() {
-		
+
+		double usos=0;
+		double valorArrecadado=0;
+
+		for(Cliente x:clientes){
+			usos+=x.totalDeUsos();
+			valorArrecadado+=x.arrecadadoTotal();	
+		}	
+		double media = valorArrecadado/usos;
 	}
 
 	public String top5Clientes(int mes) {
+		
 		
 	}
 
