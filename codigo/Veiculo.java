5c8721033430db8b472c7ff6395b3591a0640b8e
Gabriel Resende <gabriellrr2005@gmail.com>
2023-10-04 21:59:30 -0300

    métodos implementados

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,32 +1,48 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Veiculo {
 
-	private String placa;
-	private UsoDeVaga[] usos;
+    private String placa;
+    private List<UsoDeVaga> usos;
 
-	public Veiculo(String placa) {
-		
-	}
+    public Veiculo(String placa) {
+        this.placa = placa;
+        usos = new ArrayList<>();
+    }
 
-	public void estacionar(Vaga vaga) {
-		
-	}
+    public void estacionar(Vaga vaga) {
+        if (vaga.disponivel()) {
+            usos.add(new UsoDeVaga(vaga));
+        }
+    }
 
-	public double sair() {
-		
-	}
+    public double sair() {
+        int ultimaPosicao = usos.size() - 1;
+        UsoDeVaga uso = usos.get(ultimaPosicao);
+        uso.sair();
+        return uso.valorPago();
+    }
 
-	public double totalArrecadado() {
-		
-	}
+    public double totalArrecadado() {
+        double totalPago = 0;
+        for (UsoDeVaga uso : usos) {
+            totalPago += uso.valorPago();
+        }
+        return totalPago;
+    }
 
-	public double arrecadadoNoMes(int mes) {
-		
-	}
+    public double arrecadadoNoMes(int mes) {
+        double arrecadacao = 0d;
+        for (UsoDeVaga uso : usos) {
+            if (uso.ehDoMes(mes)) {
+                arrecadacao += uso.valorPago();
+            }
+        }
+        return arrecadacao;
+    }
 
-	public int totalDeUsos() {
-		
-	}
-
+    public int totalDeUsos() {
+        return usos.size();
+    }
 }
