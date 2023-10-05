0cd6092d59e44c4475e0d89ca8f6b8af4c990bc9
jvcostapuc <joao.costa.1453732@sga.pucminas.br>
2023-10-04 22:23:59 -0300

    estacionamento parte 2

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -25,19 +25,51 @@ public class Estacionamento {
 	}
 
 	public void estacionar(String placa) {
-		
-	}
+		Veiculo veiculo = procuraVeiculo(placa);
+		Vaga vaga = procuraVaga();
 
+		if(vaga != null && veiculo != null){
+			veiculo.estacionar(vaga);
+		}
+	}
+	private Vaga procuraVaga(){
+		for(Vaga vaga : vagas){
+			if(vaga.disponivel()){
+				return vaga;
+			}
+		}
+		return null;
+	}
+	private Veiculo procuraVeiculo(String placa){
+		for(Cliente cliente: id){
+			Veiculo veiculo = cliente.possuiVeiculo(placa);
+			return veiculo;
+		}
+		return null;
+	}
 	public double sair(String placa) {
-		
+		Veiculo veiculo = procuraVeiculo(placa);
+		return veiculo.sair();
 	}
 
 	public double totalArrecadado() {
-		
+		double total = 0;
+		for(Cliente cliente: id){
+			if(cliente != null){
+				total += cliente.arrecadadoTotal();
+			}
+		}
+		return total;
 	}
 
 	public double arrecadacaoNoMes(int mes) {
-		
+		double totalMes = 0;
+		for(Cliente cliente: id){
+			if(cliente != null){
+				totalMes += cliente.arrecadadoNoMes(mes);
+			}
+		}
+		return totalMes;
 	}
 
 	public double valorMedioPorUso() {
