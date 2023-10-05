d681baa54596d99ec4d7fcd245ca790743ccda6d
Gabriel Resende <gabriellrr2005@gmail.com>
2023-10-04 22:26:27 -0300

    documentação

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,22 +1,38 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe para guardar atributos e realizar ações referentes ao Veículo
+ */
 public class Veiculo {
 
     private String placa;
     private List<UsoDeVaga> usos;
 
+    /**
+     * Construtor para criar um veículo a partir de sua placa
+     * @param placa
+     */
     public Veiculo(String placa) {
         this.placa = placa;
         usos = new ArrayList<>();
     }
 
+    /**
+     * Se a vaga passada por parâmetro estiver disponível é adicionada
+     * um uso de vaga para a lista de usos de vaga.
+     * @param vaga
+     */
     public void estacionar(Vaga vaga) {
         if (vaga.disponivel()) {
             usos.add(new UsoDeVaga(vaga));
         }
     }
 
+    /**
+     * Chama o metodo de sair da classe UsoDeVaga e retorna o valor pago para esse uso.
+     * @return
+     */
     public double sair() {
         int ultimaPosicao = usos.size() - 1;
         UsoDeVaga uso = usos.get(ultimaPosicao);
@@ -24,6 +40,10 @@ public class Veiculo {
         return uso.valorPago();
     }
 
+    /**
+     * Calcula e retorna o total de valores de todos os usos de vaga.
+     * @return totalArrecadado nos usos
+     */
     public double totalArrecadado() {
         double totalPago = 0;
         for (UsoDeVaga uso : usos) {
@@ -32,6 +52,11 @@ public class Veiculo {
         return totalPago;
     }
 
+    /**
+     * Calcula o total arrecado em um determinado mes
+     * @param mes
+     * @return totalArrecadado no mes
+     */
     public double arrecadadoNoMes(int mes) {
         double arrecadacao = 0d;
         for (UsoDeVaga uso : usos) {
@@ -42,6 +67,10 @@ public class Veiculo {
         return arrecadacao;
     }
 
+    /**
+     * Retorna o total de usos de vagas
+     * @return totalDeUsos
+     */
     public int totalDeUsos() {
         return usos.size();
     }
