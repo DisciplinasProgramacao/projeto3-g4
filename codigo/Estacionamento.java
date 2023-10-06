d800c7f13686aa80359f51f6a9f1bab5b0f7fd8b
pedro <pedromaximocc@gmail.com>
2023-10-06 10:35:57 -0300

    aprendendo interface e lambda para ordernar

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Estacionamento {
@@ -49,12 +50,11 @@ public class Estacionamento {
 		String id;
     for (int i = 0; i < quantFileiras; i++) {
       for (int j = 0; j < vagasPorFileira; j++){ 
-						
-            id = letras[i] + String.valueOf(numeros[j]);
-						Vaga x = new Vaga(i,j,id);
-						vagas.add(x);
-            
-					
+				
+        id = letras[i] + String.valueOf(numeros[j]);
+				Vaga x = new Vaga(i,j,id);
+				vagas.add(x);
+        
         }
     }
 	}
@@ -89,8 +89,38 @@ public class Estacionamento {
 	}
 
 	public String top5Clientes(int mes) {
-		
-		
-	}
+		List<Cliente> clienteDoMes = new ArrayList<>();
+	
+		for (Cliente x : clientes) {
+			if(x.arrecadadoNoMes(mes)>0){
+					clienteDoMes.add(x);
+			}
+		}
+		clienteDoMes.sort(new Comparator<Cliente>(){// utiliza o sort para ordernar a lista clienteDoMes; Utiliza a interface Comparator para estabelecer uma regra de comparação, realizando uma instanciação anonima;
+        @Override//reescrevemos a regra compare da interface comparator
+        public int compare(Cliente cliente1, Cliente cliente2) {// utiliza o metodo compare da interface comparator (no qual se der negativo cliente1 vem antes do 2 , se der zero sao iguais e se der positivo cliente1 vem depois do 2)
+            double arrecadacao1 = cliente1.arrecadadoNoMes(mes);
+            double arrecadacao2 = cliente2.arrecadadoNoMes(mes);
+            // Ordene em ordem decrescente
+            return Double.compare(arrecadacao2, arrecadacao1);//ordenamos o compare dessa maneira para podermos ter a Lista decrescente. Ex.: se arrecadaçao cliente 2 > arrecadaçao cliente1 = +1 logo cliente1 vem depois do cliente2;
+        }
+    });
 
+		int maxClientes = Math.min(5, clienteDoMes.size());//Math.min estabelece que o tamanho maximo da lista seja 5, 
+
+		StringBuilder top5 = new StringBuilder();
+
+		top5.append("Top 5 clientes do mês "+ mes+" :\n");
+
+		for(int i=0;i<maxClientes;i++){
+				Cliente cliente = clienteDoMes.get(i);
+				top5.append(cliente+"\n");//utiliza o metodo toString() da classe cliente; 
+			}
+
+		return top5.toString();
+	
+	}
+		
 }
+
+
